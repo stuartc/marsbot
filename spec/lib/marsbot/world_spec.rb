788c0c89fe7e064dbacb261d283c0e64124d31c3
Stuart Corbishley <corbish@gmail.com>
2018-07-11 12:36:43 +0200

    World no longer tracks

--- spec/lib/marsbot/world_spec.rb
+++ spec/lib/marsbot/world_spec.rb
@@ -19,36 +19,4 @@ describe Marsbot::World do
   #   end
   # end
 
-  describe 'tracker' do
-    let(:robots) do
-      [
-        Marsbot::Robot.new(
-          x: 1, y: 1, orientation: "E",
-          instructions: "RFRFRFRF".chars
-        ),
-        Marsbot::Robot.new(
-          x: 3, y: 2, orientation: "N",
-          instructions: "FRRFLLFFRRFLL".chars
-        ),
-        Marsbot::Robot.new(
-          x: 0, y: 3, orientation: "W",
-          instructions: "LLFFFLFLFL".chars
-        )
-      ]
-    end
-
-    before do
-      robots.each do |robot|
-        world << robot
-      end
-    end
-
-    it do
-      world.robots.map do |robot|
-        puts "#{robot.coordinates.join(' ')} #{robot.orientation}"
-      end
-      expect(world.tracker.out_of_bounds).to contain_exactly(robots[1])
-
-    end
-  end
 end
